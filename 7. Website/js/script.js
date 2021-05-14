50e860d076d147e4f2025c055c60ffcfe8c38bff
zhulym <zhulym@mail.ru>
2021-05-14 23:44:38 +0300

    added section service

--- 7. Website/js/script.js
+++ 7. Website/js/script.js
@@ -1,2 +1,18 @@
+const headerLinks = document.querySelectorAll('.header__link');
+
+headerLinks.forEach(item => {
+    item.addEventListener('click', addActiveLink);
+})
+
+function addActiveLink(event) {
+    headerLinks.forEach(item => {
+        item.classList.remove('header__active');
+    })
+    if (event.target.classList.contains('header__active')) {
+        return;
+    } else {
+        event.target.classList.add('header__active');
+    }
+}
 
 
