cd008fdc179e02ad2e89efdad8e97643ed7686e6
zhulym <zhulym@mail.ru>
2021-05-15 18:39:51 +0300

    slider

--- 7. Website/js/script.js
+++ 7. Website/js/script.js
@@ -1,10 +1,42 @@
 const headerLinks = document.querySelectorAll('.header__link');
+const contactsDate = document.querySelector('.contacts__date');
+const leftArrow = document.querySelector('.header__left-arrow');
+const rightArrow = document.querySelector('.header__right-arrow');
+
+
+
+const slides = document.querySelectorAll('.header__slide');
+let currentSlide = 0;
+
+rightArrow.addEventListener('click', slideBlockRight);
+
+function slideBlockRight() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.add('hide_slides');
+    }
+    slides[currentSlide].classList.remove('hide_slides');
+    currentSlide++;
+
+    if (currentSlide++ === slides.length) {
+        currentSlide = 0;
+    }
+
+}
+
+
+
+
+
+
+
+
 
 headerLinks.forEach(item => {
     item.addEventListener('click', addActiveLink);
 })
 
 function addActiveLink(event) {
+    debugger;
     headerLinks.forEach(item => {
         item.classList.remove('header__active');
     })
@@ -14,5 +46,10 @@ function addActiveLink(event) {
         event.target.classList.add('header__active');
     }
 }
+
+
+
+let dateForBlog = new Date();
+contactsDate.innerHTML = `${dateForBlog.getFullYear()}-${dateForBlog.getMonth()}-${dateForBlog.getDate()} &nbsp; ${dateForBlog.getHours()}:${dateForBlog.getMinutes()}`;
 
 
