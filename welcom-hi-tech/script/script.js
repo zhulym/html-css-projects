9aeea007ebc5a4fd102947cc5b95770282d0f5e9
zhulym <zhulym@mail.ru>
2021-08-19 21:23:01 +0300

    refactor: replace projectd to other branch

--- welcom-hi-tech/script/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('.heading__burger').click(function (event) {
-        $('.heading__burger, .header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
-});
